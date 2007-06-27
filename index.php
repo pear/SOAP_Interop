bb1b166c176e6b787d9f8550890501a62562cf69
Christian Weiske <cweiske@php.net>
2007-06-27 06:41:36 +0000

    - Fix Bug #11454: basedir config is not respected in index.php - Replace & -> &amp; to get valid html
    git-svn-id: http://svn.php.net/repository/pear/packages/SOAP_Interop/trunk@238691 c90b9560-bf6c-de11-be94-00142212c4b1

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>PEAR SOAP Interop</title>
 </head>
 <?php
+require_once 'config.php';
 require_once 'registrationAndNotification.php';
 
 $tests = array('Round 2 Base',
@@ -21,7 +22,7 @@ $tests = array('Round 2 Base',
         );
 
 // get our endpoint
-$baseurl = 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].'/soap_interop/';
+$baseurl = 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].$interopConfig['basedir'];
 ?>
 <body>
 
@@ -71,26 +72,26 @@ WSDLCACHE errors mean we cannot retreive the WSDL file specified for the endpoin
 </p>
 
 <ul>
-<li><a href="interop_client_results.php?test=Round+2+Base&type=php&wsdl=0">Base results using PHP native types</a></li>
-<li><a href="interop_client_results.php?test=Round+2+Base&type=soapval&wsdl=0">Base results using SOAP types</a></li>
-<li><a href="interop_client_results.php?test=Round+2+Base&type=php&wsdl=1">Base results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+2+Base&amp;type=php&amp;wsdl=0">Base results using PHP native types</a></li>
+<li><a href="interop_client_results.php?test=Round+2+Base&amp;type=soapval&amp;wsdl=0">Base results using SOAP types</a></li>
+<li><a href="interop_client_results.php?test=Round+2+Base&amp;type=php&amp;wsdl=1">Base results using PHP native types with WSDL</a></li>
 
-<li><a href="interop_client_results.php?test=Round+2+Group+B&type=php&wsdl=0">Group B results using PHP native types</a></li>
-<li><a href="interop_client_results.php?test=Round+2+Group+B&type=soapval&wsdl=0">Group B results using SOAP types</a></li>
-<li><a href="interop_client_results.php?test=Round+2+Group+B&type=php&wsdl=1">Group B results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+2+Group+B&amp;type=php&amp;wsdl=0">Group B results using PHP native types</a></li>
+<li><a href="interop_client_results.php?test=Round+2+Group+B&amp;type=soapval&amp;wsdl=0">Group B results using SOAP types</a></li>
+<li><a href="interop_client_results.php?test=Round+2+Group+B&amp;type=php&amp;wsdl=1">Group B results using PHP native types with WSDL</a></li>
 
-<li><a href="interop_client_results.php?test=Round+2+Group+C&type=php&wsdl=0">Group C results using PHP native types</a></li>
-<li><a href="interop_client_results.php?test=Round+2+Group+C&type=soapval&wsdl=0">Group C results using SOAP types</a></li>
-<li><a href="interop_client_results.php?test=Round+2+Group+C&type=php&wsdl=1">Group C results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+2+Group+C&amp;type=php&amp;wsdl=0">Group C results using PHP native types</a></li>
+<li><a href="interop_client_results.php?test=Round+2+Group+C&amp;type=soapval&amp;wsdl=0">Group C results using SOAP types</a></li>
+<li><a href="interop_client_results.php?test=Round+2+Group+C&amp;type=php&amp;wsdl=1">Group C results using PHP native types with WSDL</a></li>
 
-<li><a href="interop_client_results.php?test=Round+3+Group+D+Compound+1&type=php&wsdl=1">Group D Compound 1 results using PHP native types with WSDL</a></li>
-<li><a href="interop_client_results.php?test=Round+3+Group+D+Compound+2&type=php&wsdl=1">Group D Compound 2 results using PHP native types with WSDL</a></li>
-<li><a href="interop_client_results.php?test=Round+3+Group+D+DocLit&type=php&wsdl=1">Group D DocLit results using PHP native types with WSDL</a></li>
-<li><a href="interop_client_results.php?test=Round+3+Group+D+DocLitParams&type=php&wsdl=1">Group D DocLitParams results using PHP native types with WSDL</a></li>
-<li><a href="interop_client_results.php?test=Round+3+Group+D+Import+1&type=php&wsdl=1">Group D Import 1 results using PHP native types with WSDL</a></li>
-<li><a href="interop_client_results.php?test=Round+3+Group+D+Import+2&type=php&wsdl=1">Group D Import 2 results using PHP native types with WSDL</a></li>
-<li><a href="interop_client_results.php?test=Round+3+Group+D+Import+3&type=php&wsdl=1">Group D Import 3 results using PHP native types with WSDL</a></li>
-<li><a href="interop_client_results.php?test=Round+3+Group+D+RpcEnc&type=php&wsdl=1">Group D RpcEnc results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+3+Group+D+Compound+1&amp;type=php&amp;wsdl=1">Group D Compound 1 results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+3+Group+D+Compound+2&amp;type=php&amp;wsdl=1">Group D Compound 2 results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+3+Group+D+DocLit&amp;type=php&amp;wsdl=1">Group D DocLit results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+3+Group+D+DocLitParams&amp;type=php&amp;wsdl=1">Group D DocLitParams results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+3+Group+D+Import+1&amp;type=php&amp;wsdl=1">Group D Import 1 results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+3+Group+D+Import+2&amp;type=php&amp;wsdl=1">Group D Import 2 results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+3+Group+D+Import+3&amp;type=php&amp;wsdl=1">Group D Import 3 results using PHP native types with WSDL</a></li>
+<li><a href="interop_client_results.php?test=Round+3+Group+D+RpcEnc&amp;type=php&amp;wsdl=1">Group D RpcEnc results using PHP native types with WSDL</a></li>
 
 <li><a href="interop_client_results.php">Show All Results</a></li>
 </ul>
